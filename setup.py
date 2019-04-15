4823e715bf0dfd834da51160c1ad84031f96a81e
Jim Arnold <jimmyjamesarnold@gmail.com>
2019-04-15 12:05:38 -0400

    changed module layout

--- setup.py
+++ setup.py
@@ -12,9 +12,10 @@ setup(
     long_description=open('README.txt').read(),
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     install_requires=[
+        "numpy >= 1.16.2",
         "pandas >= 0.24.2",
         "Biopython >= 1.73",
         "openpyxl >= 2.6.1",
-        "requests >= 2.21.0"
+        "requests >= 2.21.0",
     ],
 )
